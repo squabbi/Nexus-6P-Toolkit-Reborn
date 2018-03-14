4c601945913f1920eaed9223c4dfe6426ff51866
Squabbi <squabbi@outlook.com>
2018-03-15 09:28:44 +1100

    Started JSON deserialisation for Magisk

--- Nexus 6P Toolkit Reborn/MainWindow.xaml.cs
+++ Nexus 6P Toolkit Reborn/MainWindow.xaml.cs
@@ -24,6 +24,27 @@ namespace Nexus_6P_Toolkit_Reborn
             InitializeComponent();
         }
 
+        public class JsonResults
+        {
+            public Magisk _Magisk { get; set; }
+            public MagiskManager _MagiskManager { get; set; }
+        }
+
+        public class MagiskManager
+        {
+            public string version;
+            public int versionCode;
+            public string link;
+        }
+
+        public class Magisk
+        {
+            public string version;
+            public int versionCode;
+            public string link;
+            public string note;
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
